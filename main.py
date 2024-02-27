5209a25b8798517a8dc1b8ab9ad144d476cdbe45
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-02-27 12:56:46 +0000

    Realizar ejercicio

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+"""1º Qué palabra reservada hay que utilizar en lugar de animal (nombre de la clase) para acceder al atributo patas.PendingDeprecationWarning
+
+2º Qué palabra reservada hay que utilizar para crear un nuevo objeto"""
+
 class animal:
-    patas = 4
+    patas = 0
     
     def caminar():
-        print("Caminando")
+        print(f"Caminando con {animal.patas} patas")
 
 def main():
     vaca = animal
+    vaca.patas = 4
     vaca.caminar()
+
+    pato = animal
+    vaca.patas = 2
+    pato.caminar()
 
 if __name__ == "__main__":
     main()
