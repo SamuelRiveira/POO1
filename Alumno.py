a63c633c44694785edc51c568ef59c53828804cc
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-02-28 09:29:58 +0000

    __init__

--- Alumno.py
+++ Alumno.py
@@ -1,11 +1,12 @@
 class alumno:
+    def __init__(self):
+        self.nota = 0
+        self.nombre = ""
+
     def imprimir(self):
-        self.nombre = ""
-        self.nota = 0
         print(f"La nota del alumno {self.nombre} es {self.nota}")
+    
     def promocion(self):
-        self.nombre = ""
-        self.nota = 0
         if self.nota >= 5:
             print(f"El alumno {self.nombre} está promociona")
         else:
